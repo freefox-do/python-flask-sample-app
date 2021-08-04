91d6f29927cfd0cba6e9e3efc6f2105af4006440
xinzhao219 <53591193+xinzhao219@users.noreply.github.com>
2021-08-04 22:50:25 +1200

    Delete metric-filter.sh

--- aws-log/metric-filter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws logs put-metric-filter \
---log-group-name post-log \
---filter-name ByContentSize \
---filter-pattern '[timestamp, size, user_id]' \
---metric-transformations \
-'metricName=ByContentSize,metricNamespace=AppLog,metricValue=$size,defaultValue=0' --region ap-southeast-2
