2a27e538da8e4dd8032638e4508dc3a69c405212
xinzhao219 <53591193+xinzhao219@users.noreply.github.com>
2021-08-05 13:18:52 +1200

    Update blog.py

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -85,7 +85,7 @@ def create():
             dateTimeObj = datetime.now()
             size = len(body)
             with open('/var/log/post.log','a') as f:
-                f.write(str('\n'+"timestamp:")+str(dateTimeObj)+str(",size:")+str(size)+str(",user_id:")+str(id)+'\n')
+                f.write(str("timestamp:")+str(dateTimeObj)+str(",size:")+str(size)+str(",user_id:")+str(id)+'\n')
             """end: Write to post-log file"""
             
             return redirect(url_for("blog.index"))
@@ -120,7 +120,7 @@ def update(id):
             dateTimeObj = datetime.now()
             size = len(body)
             with open('/var/log/post.log','a') as f:
-                f.write('\n'+str("timestamp:")+str(dateTimeObj)+str(",size:")+str(size)+str(",user_id:")+str(id))
+                f.write(str("timestamp:")+str(dateTimeObj)+str(",size:")+str(size)+str(",user_id:")+str(id)+'\n')
             """end: Write to post-log file"""
 
             return redirect(url_for("blog.index"))
