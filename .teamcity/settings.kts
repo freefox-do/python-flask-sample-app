929954bf41a94329bfc7ea680cd16dad48f12eb7
wellyfox <freefox@gmail.com>
2022-09-23 20:36:12 +1200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -17,25 +17,25 @@ project {
     vcsRoot(VCSExample)
 
 
-    buildType(DockerBuildsteve)
+ //   buildType(DockerBuildsteve)
 
 }
 
-object DockerBuildsteve : DockerBuild({
-    name = "docker build"
+//object DockerBuildsteve : DockerBuild({
+ //   name = "docker build"
 
-    vcs {
-        root(VCSExample)
-    }
+//    vcs {
+//        root(VCSExample)
+//    }
 
 
-    repository = "13.239.84.133:8082"
-    imageName = "python-flask-sample-app"
-    tag = "v1.0.0"
-    artusername = "admin"
-    artpassword = "credentialsJSON:4365953f-8420-45b5-bebe-924c1243cd0a"
+//    repository = "13.239.84.133:8082"
+//    imageName = "python-flask-sample-app"
+//    tag = "v1.0.0"
+//    artusername = "admin"
+//    artpassword = "credentialsJSON:4365953f-8420-45b5-bebe-924c1243cd0a"
 
-})
+//})
 
 
 object VCSExample : GitVcsRoot({
